de409db2d8fb708a8dd39595e50e75defc39e151
Ecky819 <marcoeggert73@gmail.com>
2025-09-22 11:50:13 +0200

    Android Änderungen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.projekt_unbarmherzigkeit"
-        minSdk = 21  // Mindestens 21 für Firebase
+        minSdk = flutter.minSdkVersion  // Mindestens 21 für Firebase
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
